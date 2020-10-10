1afdefb6cd689a6d5d51dbeff8ba0f1464bd1674
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2020-10-10 16:49:03 -0300

    Java Spring Boot - JPA - Hibernate - H2 - One To Many Bidirectional Relationship - @OneToMany

---
+++ jpa-one-to-many-bidirectional/src/main/java/com/lduran/jpaonetomanybidirectional/JpaOneToManyBidirectionalApplication.java
@@ -0,0 +1,13 @@
+package com.lduran.jpaonetomanybidirectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToManyBidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOneToManyBidirectionalApplication.class, args);
+	}
+
+}
---
+++ jpa-one-to-many-bidirectional/src/test/java/com/lduran/jpaonetomanybidirectional/JpaOneToManyBidirectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lduran.jpaonetomanybidirectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOneToManyBidirectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
